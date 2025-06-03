9783fecc56533d8cef96702aaf441ee63cb28c98
Lucas Pedro <botdanet38@gmail.com>
2025-06-02 23:39:18 -0300

    Adiciona funcionalidade para os botões de trocar tema, além de salvar no local storage o tema selcionado

---
+++ trocarTemas.js
@@ -0,0 +1,29 @@
+let temaAtual = localStorage.getItem('tema')
+const trocarTema = document.getElementById('trocar-tema')
+
+const habilitarModoClaro = () => {
+    document.body.classList.add('modo-claro')
+    document.body.classList.remove('modo-escuro')
+    localStorage.setItem('tema', 'claro')
+}
+
+const habilitarModoEscuro = () => {
+    document.body.classList.add('modo-escuro')
+    document.body.classList.remove('modo-claro')
+    localStorage.setItem('tema', 'escuro')
+}
+
+if (temaAtual === 'claro') {
+    habilitarModoClaro()
+} else {
+    habilitarModoEscuro()
+}
+
+trocarTema.addEventListener("click", () => {
+    temaAtual = localStorage.getItem('tema')
+    if (temaAtual === 'claro') {
+        habilitarModoEscuro()
+    } else {
+        habilitarModoClaro()
+    }
+})
