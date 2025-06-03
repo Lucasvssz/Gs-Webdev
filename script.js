056ebb9c8e337010efca172a5bbe94b8ce3c843b
murilo <murilocraft5@gmail.com>
2025-06-02 23:44:20 -0300

    Adicionando slide-show em html css e script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Olá")
+let slideIndex = 0;
+let slides = document.getElementsByClassName("mySlides");
+let timer;
+
+function showDivsAuto() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+
+  slides[slideIndex - 1].style.display = "block";
+  timer = setTimeout(showDivsAuto, 4000); // troca a cada 4 segundos
+}
+
+function plusDivs(n) {
+  clearTimeout(timer); // para o auto temporariamente
+  slideIndex += n - 1;
+  if (slideIndex < 0) { slideIndex = slides.length - 1; }
+  showDivsAuto(); // reinicia a partir do novo índice
+}
+
+window.onload = showDivsAuto;
